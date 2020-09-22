5f3e5eb977c979773c8d366cf4f824a5f2f4953e
MartyRichards <m.petitpre@hotmail.fr>
2020-09-22 17:06:53 +0200

    commit2

--- TD2/app/src/main/java/com/example/td2/MainActivity.java
+++ TD2/app/src/main/java/com/example/td2/MainActivity.java
@@ -9,6 +9,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        //setContentView(R.layout.password);
+       //setContentView(R.layout.calendar);
+        setContentView(R.layout.relativelayout);
+
+
+
     }
 }
